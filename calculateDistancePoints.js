f385aff862805d90a7575762d40f7d5f9e0c6954
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-24 18:16:11 +0200

    Add calculate Distance and Style points

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,3 +1,29 @@
-const calculateDistancePoints = (distance, hillSize, kPoint) => {};
+const calculateDistancePoints = (distance, hillSize, kPoint) => {
+  if (!distance || !hillSize || kPoint) {
+    throw Error("There is at least one undefined argument");
+  }
+  let pointsDifferenceNormal = (distance - kPoint) * 2;
+  let pointsDifferenceLarge = (distance - kPoint) * 1.8;
+  let pointsDifferenceFlying = (distance - kPoint) * 1.2;
+
+  switch (
+    hillSize
+      .toString()
+      .trim()
+      .toLowerCase()
+  ) {
+    case "normal":
+      return 60 + pointsDifferenceNormal;
+
+    case "large":
+      return 60 + pointsDifferenceLarge;
+
+    case "flying":
+      return 120 + pointsDifferenceFlying;
+
+    default:
+      throw Error(`${hillSize} is not a correct hill size`);
+  }
+};
 
 module.exports = calculateDistancePoints;
--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,3 +1,15 @@
-const calculateStylePoints = styleNotes => {};
+const calculateStylePoints = styleNotes => {
+  const stylePoints = styleNotes;
+
+  stylePoints.sort((a, b) => {
+    return a - b;
+  });
+  stylePoints.slice(1, 4);
+  stylePoints.reduce((acc, cur) => {
+    return acc + cur;
+  });
+
+  return stylePoints;
+};
 
 module.exports = calculateStylePoints;
